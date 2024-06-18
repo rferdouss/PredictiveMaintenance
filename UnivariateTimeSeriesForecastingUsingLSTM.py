e3d73fe7d761ddb1fdb6e0ea6d04b609350b2029
Raihana Ferdous <84193541+rferdouss@users.noreply.github.com>
2024-06-18 20:51:53 +0200

    Update UnivariateTimeSeriesForecastingUsingLSTM.py
    added multivariate time series forecasting method

--- UnivariateTimeSeriesForecastingUsingLSTM.py
+++ UnivariateTimeSeriesForecastingUsingLSTM.py
@@ -46,13 +46,13 @@ from sklearn.metrics import mean_absolute_percentage_error
 
 from math import sqrt
 from statsmodels.tsa.holtwinters import ExponentialSmoothing        # for Holt-Winters Exponential Smoothing
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.layers import TimeDistributed
-from keras.layers.convolutional import Conv1D
-from keras.layers.convolutional import MaxPooling1D
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import TimeDistributed
+from tensorflow.keras.layers import Conv1D
+from tensorflow.keras.layers import MaxPooling1D
 
 
 
@@ -134,7 +134,80 @@ def split_sequence(sequence, n_steps):
     return np.array(X), np.array(y)
 
 
+#----------------------------------------------------------------------------------------------------------------
+# Univariate time series forecasting using LSTM. This function formulate the univariate time series into supervised
+# learning problem and then use LSTM to predict one step (e.g., one week) value for number of alerts/critical alerts
+#----------------------------------------------------------------------------------------------------------------
+def UnivariateTimeSeriesForecastingUsingLSTM(subdfTrainData, varname):
+#print(subdfTrainData.head(10))
+    raw_seq = subdfTrainData[varname].to_numpy()
+    print('length of array = ', len(raw_seq))
+    print('data = ', raw_seq)
 
+
+    #-------------Univariate time series forecasting using LSTM models------------------------------------
+
+    # define input sequence
+    #raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+    # choose a number of time steps
+    n_steps = 3
+    # split into samples
+    X, y = split_sequence(raw_seq, n_steps)
+    # reshape from [samples, timesteps] into [samples, timesteps, features]
+    n_features = 1
+    X = X.reshape((X.shape[0], X.shape[1], n_features))
+    # summarize the data
+    for i in range(len(X)):
+        print(X[i], y[i])
+
+    # define model
+    model = Sequential()
+    model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    print('Model')
+    # fit model
+    model.fit(X, y, epochs=200, verbose=0)
+    # demonstrate prediction
+    x_input = np.array(subdfTrainData[varname][-n_steps:]) # get the last n value (number of sequence)
+    #x_input = x_input.reshape((1, n_steps, n_features))
+    x_input = x_input.reshape((1, n_steps, n_features))
+    yhat = model.predict(x_input, verbose=0)
+    print('Prediction val = ', yhat)
+
+#----------------------------------------------------------------------------------------------------------------
+# Univariate time series forecasting using LSTM. This function formulate the univariate time series into supervised
+# learning problem and then use LSTM to predict one step (e.g., one week) value for number of alerts/critical alerts
+#----------------------------------------------------------------------------------------------------------------
+def MultivariateTimeSeriesForecastingUsingLSTM(subdfTrainData, varname):
+    # define input sequence
+    in_seq1 = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+    in_seq2 = np.array([15, 25, 35, 45, 55, 65, 75, 85, 95])
+    out_seq = np.array([in_seq1[i] + in_seq2[i] for i in range(len(in_seq1))])
+    # convert to [rows, columns] structure
+    in_seq1 = in_seq1.reshape((len(in_seq1), 1))
+    in_seq2 = in_seq2.reshape((len(in_seq2), 1))
+    out_seq = out_seq.reshape((len(out_seq), 1))
+    # horizontally stack columns
+    dataset = np.hstack((in_seq1, in_seq2, out_seq))
+    # choose a number of time steps
+    n_steps = 3
+    # convert into input/output
+    X, y = split_sequence(dataset, n_steps)
+    # the dataset knows the number of features, e.g. 2
+    n_features = X.shape[2]
+    # define model
+    model = Sequential()
+    model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    # fit model
+    model.fit(X, y, epochs=200, verbose=0)
+    # demonstrate prediction
+    x_input = np.array([[80, 85], [90, 95], [100, 105]])
+    x_input = x_input.reshape((1, n_steps, n_features))
+    yhat = model.predict(x_input, verbose=0)
+    print(yhat)
 
 #------------------------------------------------------------------------------------
 # Press the green button in the gutter to run the script.
@@ -177,34 +250,6 @@ if __name__ == '__main__':
     dfTrainData = pd.DataFrame.from_dict(TrainDataRecord, orient='index')
     subdfTrainData = dfTrainData[['NumAlerts', 'NumCriticalAlerts']]  # Create new pandas DataFrame
     varname = 'NumAlerts'  #'NumAlerts', 'NumCriticalAlerts'
-    #print(subdfTrainData.head(10))
 
-    #-------------Univariate time series forecasting using LSTM models------------------------------------
-
-    # define input sequence
-    raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-    # choose a number of time steps
-    n_steps = 3
-    # split into samples
-    X, y = split_sequence(raw_seq, n_steps)
-    # summarize the data
-    for i in range(len(X)):
-        print(X[i], y[i])
-
-    # define model
-    # reshape from [samples, timesteps] into [samples, subsequences, timesteps, features]
-    n_features = 1
-    n_seq = 2
-    n_steps = 2
-
-    model = Sequential()
-    model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
-    model.add(Dense(1))
-    model.compile(optimizer='adam', loss='mse')
-    # fit model
-    model.fit(X, y, epochs=200, verbose=0)
-    # demonstrate prediction
-    x_input = np.array([70, 80, 90])
-    x_input = x_input.reshape((1, n_steps, n_features))
-    yhat = model.predict(x_input, verbose=0)
-    print('Prediction val = ', yhat)
+    #UnivariateTimeSeriesForecastingUsingLSTM(subdfTrainData, varname)
+    MultivariateTimeSeriesForecastingUsingLSTM(subdfTrainData, varname)
